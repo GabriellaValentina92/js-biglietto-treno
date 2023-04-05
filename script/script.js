ec7deda613a70631ed29c01d01db0108f7da3c64
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-05 17:59:00 +0200

    Add isNan

--- script/script.js
+++ script/script.js
@@ -1,30 +1,26 @@
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
-
 let km_number = parseInt(prompt('Inserire il numero dei kilometri da percorrere'));
 
 let age = parseInt(prompt('Inserire età del passeggero'));
 
 let price = (km_number * 0.21);
-console.log(price)
 
 let discountYoung = price - (price * 20 / 100);
-console.log(discountYoung)
 let discountaged = price - (price * 40 / 100);
-console.log(discountaged)
 
 let discount = discountYoung.toFixed(2)
 let Discount = discountaged.toFixed(2)
 
-if (age < 18){
-    document.getElementById('ticket').innerHTML = discount;
-} else if (age > 65){
-    document.getElementById('ticket').innerHTML = Discount;
-}else {
-    document.getElementById('ticket').innerHTML = price;
+let value = (' il valore inserito non è un numero')
+
+if (isNaN(km_number) || isNaN(age)) {
+	document.getElementById('ticket').innerHTML = value;
+} else {
+    if (age < 18){
+        document.getElementById('ticket').innerHTML = discount;
+    } else if (age > 65){
+        document.getElementById('ticket').innerHTML = Discount;
+    }else {
+        document.getElementById('ticket').innerHTML = price;
+    }
 }
